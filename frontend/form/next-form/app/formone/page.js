4afbb016749e2849c0e96f35097a229318a41949
plutoese <glen.zhang7@gmail.com>
2023-10-22 09:49:27 +0800

    reack hook form v0.1

---
+++ frontend/form/next-form/app/formone/page.js
@@ -0,0 +1,19 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName")} />
+      <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+      </select>
+      <input type="submit" />
+    </form>
+  );
+}
---
+++ frontend/form/next-form/app/page.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-3xl m-5">Hello React Hook Form</h1>;
+}
